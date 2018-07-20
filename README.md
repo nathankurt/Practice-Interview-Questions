1122c4fb873db76a638a48152887e9959b2a95c9
Nathan Kurt <nathankurt@quickenloans.com>
2018-07-20 12:40:40 -0400

    Added their answer to doc and added problem 2 readme

--- README.md
+++ README.md
@@ -39,10 +39,28 @@ Given a list of tuples like `points_tuple = [(-2,4), (0,-2), (-1,0), (2,5), (-2,
 
 ## Daily Coding Problems
 
-### Problem 1(7/19/18)
+### (7/19/18) Problem 1
 
 Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
 For example, given `[10, 15, 3, 7]` and `k of 17`, `return true` since 10 + 7 is 17.
 
 **Bonus**: Can you do this in one pass?
+
+#### Solution
+
+This problem can be solved in several different ways.
+
+1. Brute force way would involve a nested iteration to check for every pair of numbers. This would take O(N^2).
+
+2. use a set to remember the numbers we've seen so far. Then for a given number, we can check if there is another number that, if added, would sum to k. This would be O(N) since lookups of sets are O(1) each.
+
+3. Yet another solution involves sorting the list. We can then iterate through the list and run a binary search on K - lst[i]. Since we run binary search on N elements, this would take O(N log N) with O(1) space.
+
+### (7/20/18) Problem 2
+
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+**Follow-up:** What if you can't use division?
--- check_if_sum_of_list.py
+++ check_if_sum_of_list.py
@@ -48,7 +48,8 @@ print("\n Check Sum with Dict Comprehension")
 print("check_sum_dict(%s,%s):" % (ls,k),check_sum_dict(ls, k))
 print("check_sum_dict(%s,%s):" % (ls_big,8),check_sum_dict(ls_big, 8))
 """
-So I'm doing it this way because 
+So I'm doing it this way because dictionaries are fast and that way it is O(N) and lookups for dicts are O(1). 
+Using a set would probably be a good improvement for this case though. Sets though can't have repeats so that may not work 
 """
 def check_sum_one_pass(nums, k):
     dict_list = {}
@@ -72,3 +73,51 @@ print("check_sum_one_pass(%s,%s):" % (ls_fail,8),check_sum_one_pass(ls_fail, 8))
 print("check_sum_one_pass(%s,%s):" % (ls_big,8),check_sum_one_pass(ls_big, 8))
 #should return true
 print("check_sum_one_pass(%s,%s):" % (ls_big,10),check_sum_one_pass(ls_big, 10))
+
+
+
+""" This was the solution that they showed that seems 
+like it is probably slower than whatever I had up there."""
+
+from bisect import bisect_left
+
+
+def check_sum_site_solution(lst, K):
+    lst.sort()
+
+    for i in range(len(lst)):
+        target = K - lst[i]
+        j = binary_search(lst, target)
+
+        # Check that binary search found the target and that it's not in the same index
+        # as i. If it is in the same index, we can check lst[i + 1] and lst[i - 1] to see
+        #  if there's another number that's the same value as lst[i].
+        if j == -1:
+            continue
+        elif j != i:
+            return True
+        elif j + 1 < len(lst) and lst[j + 1] == target:
+            return True
+        elif j - 1 >= 0 and lst[j - 1] == target:
+            return True
+    return False
+
+def binary_search(lst, target):
+    lo = 0
+    hi = len(lst)
+    ind = bisect_left(lst, target, lo, hi)
+
+    if 0 <= ind < hi and lst[ind] == target:
+        return ind
+    return -1
+
+print("\n THIS IS THE SITES SOLUTION TO THE PROBLEM")
+print("check_sum_site_solution(%s,%s):" % (ls,17),check_sum_site_solution(ls, 17))
+#should return true
+print("check_sum_site_solution(%s,%s):" % (ls_dup,8),check_sum_site_solution(ls_dup, 8))
+#should return true
+print("check_sum_site_solution(%s,%s):" % (ls_fail,8),check_sum_site_solution(ls_fail, 8))
+#should return false
+print("check_sum_site_solution(%s,%s):" % (ls_big,8),check_sum_site_solution(ls_big, 8))
+#should return true
+print("check_sum_site_solution(%s,%s):" % (ls_big,10),check_sum_site_solution(ls_big, 10))
